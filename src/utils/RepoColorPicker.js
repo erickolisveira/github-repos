216a5e395bc2f95f18084696b79f8f7734c62fc6
Erick Oliveira <erickoliveiraah@gmail.com>
2020-04-24 21:54:04 -0300

    Add more colors to languages

--- src/utils/RepoColorPicker.js
+++ src/utils/RepoColorPicker.js
@@ -2,6 +2,17 @@ const languages = {
   'JavaScript': '#FFD300',
   'Java': 'orange',
   'Python': '#306998',
+  'C++': '#de3163',
+  'HTML': '#ff4700',
+  'C': '#808080',
+  'TypeScript': '#386b9b',
+  'Objective-C': '#0083a9',
+  'Ruby': '#800000',
+  'Rust': '#d2b48c',
+  'CSS': '#650065',
+  'Elixir': '#710070',
+  'PHP': '#235686',
+  
 }
 
 export default function RepoColorPicker(language) {
