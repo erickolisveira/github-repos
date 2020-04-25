25ef5ca5c8d0b0fa3738b146738b87f3176dc8aa
Erick Oliveira <erickoliveiraah@gmail.com>
2020-04-24 22:54:08 -0300

    Customize header style and create custom header component

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View, Text, Image } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator()
@@ -6,11 +7,33 @@ const Stack = createStackNavigator()
 import Main from './pages/Main'
 import Profile from './pages/Profile'
 
+function LogoTitle({ title }) {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row'}}>
+      <Image style={{ width: 25, height: 25, marginRight: 10 }} source={require('./assets/GitHub-Mark-Light-120px-plus.png')}/>
+      <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold'}}>{title}</Text>
+    </View>
+  )
+}
+
 export default function Routes() {
   return (
-    <Stack.Navigator initialRouteName="Main">
-      <Stack.Screen name="Main" component={Main} />
-      <Stack.Screen name="Profile" component={Profile} />
+    <Stack.Navigator 
+      initialRouteName="Main" 
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#000',
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          textAlign: 'center',
+        },
+      }}>
+      <Stack.Screen name="Main" component={Main}
+        options={{ headerTitle: () => <LogoTitle title="Buscar Dev"/> }} />
+      <Stack.Screen name="Profile" component={Profile} 
+        options={{ headerTitle: () => <LogoTitle title="Perfil"/> }} />
     </Stack.Navigator>
   );
 }
