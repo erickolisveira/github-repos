f01cdf1563dac3e2a734e5ee39ebf6cd8e0c61e4
Erick Oliveira <erickoliveiraah@gmail.com>
2020-04-29 23:43:00 -0300

    Copy Followers styles

---
+++ src/pages/Following/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native'
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  followerContainer: {
+    width: '100%',
+    height: 70,
+    backgroundColor: 'white',
+    elevation: 2,
+    marginBottom: 5,
+    flexDirection: 'row',
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  flatList: {
+    flex: 1,
+    width: '100%',
+    padding: 10
+  },
+  followerImage: {
+    width: '20%',
+    height: '80%',
+    borderRadius: 100,
+    resizeMode: 'contain',
+  },
+  followerUsername: {
+    fontWeight: 'bold'
+  }
+})
