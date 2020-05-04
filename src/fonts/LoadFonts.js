9d5e127d5243aa0e6afa7dcb04803c1bdd3160a3
Erick Oliveira <erickoliveiraah@gmail.com>
2020-05-04 13:36:38 -0300

    Revert "revert: Add custom font"
    This reverts commit d903f6af80307986292fc0a77ba07130861703be.

--- src/fonts/LoadFonts.js
+++ src/fonts/LoadFonts.js
@@ -2,8 +2,7 @@ import * as Font from "expo-font";
 
 export default function fetchFonts() {
   return Font.loadAsync({
-    //"Jost-Regular": require("./Jost-Regular.ttf"),
-    //"Montserrat-Medium": require("./Montserrat-Medium.ttf"),
-    //"Jost-Bold": require("./Jost-Bold.ttf"),
+    "Lato-Regular": require("./Lato-Regular.ttf"),
+    "Lato-Bold": require("./Lato-Bold.ttf"),
   })
 }
