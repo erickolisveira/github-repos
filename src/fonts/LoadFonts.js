d903f6af80307986292fc0a77ba07130861703be
Erick Oliveira <erickoliveiraah@gmail.com>
2020-05-04 13:35:49 -0300

    revert: Add custom font

--- src/fonts/LoadFonts.js
+++ src/fonts/LoadFonts.js
@@ -2,7 +2,8 @@ import * as Font from "expo-font";
 
 export default function fetchFonts() {
   return Font.loadAsync({
-    "Lato-Regular": require("./Lato-Regular.ttf"),
-    "Lato-Bold": require("./Lato-Bold.ttf"),
+    //"Jost-Regular": require("./Jost-Regular.ttf"),
+    //"Montserrat-Medium": require("./Montserrat-Medium.ttf"),
+    //"Jost-Bold": require("./Jost-Bold.ttf"),
   })
 }
