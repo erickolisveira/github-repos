cc126d9617cc6a5bf32a8181340e750031f22cb1
Erick Oliveira <erickoliveiraah@gmail.com>
2020-05-04 22:19:37 -0300

    Start work on Repository screen

--- src/screens/Repository/index.js
+++ src/screens/Repository/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect } from "react";
+
+import { Api } from "../../services";
 
 import { 
-  Container 
-} from './styles';
+  Container, 
+  Text,
+  RepoInfoView, 
+  Header, 
+  RepoStarsForksView, 
+  ViewCodeTouchable, 
+  IconStarEmpty ,
+  IconFork
+} from "./styles";
 
 export default function Repository(props) {
-  const { route: { params: { repoNode, repo} }, navigation } = props
+  const {
+    route: { params: repo },
+  } = props;
 
+  /*useEffect(() => {
+    async function getRepositoryBaseNode() {
+      const repoNode = await Api.getRepoLastCommit(repo.owner.login, repo.name);
+    }
+    getRepositoryBaseNode();
+  }, []);*/
+
+  //{ repoNode.map(node => <Text key={node.sha} >{node.path}</Text>) }
   return (
     <Container>
-      <Text>{ repo.name }</Text>
-      { repoNode.map(node => <Text key={node.sha} >{node.path}</Text>) }
+      <RepoInfoView>
+        <Text>{repo.owner.login}</Text>
+        <Header>{repo.name}</Header>
+        <Text>{repo.description}</Text>
+        <RepoStarsForksView>
+          <IconStarEmpty />
+          <Text>{repo.stargazers_count} stars</Text>
+          <IconFork />
+          <Text>{repo.forks_count} forks</Text>
+        </RepoStarsForksView>  
+      </RepoInfoView>
+      <ViewCodeTouchable>
+        <Text>Ver código</Text>
+      </ViewCodeTouchable>
     </Container>
-  )
+  );
 }
